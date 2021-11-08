69bed2fa4c934bff854ee3b0427c863b58c4b0bf
vitor2233 <vitorguilherme2233@gmail.com>
2021-11-08 20:09:48 -0300

    Adicionado adm

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ var sqlite3 = require("sqlite3").verbose();
 const DBSOURCE = "db.sqlite";
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if(err) {
+    if (err) {
         console.error(err.message);
         throw err
     } else {
@@ -14,16 +14,36 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 email text UNIQUE, 
                 senha text
             )`,
-                (err) => {
-                    if (err) {
-                        console.log(err)
-                    }else{
-                        var insert = 'INSERT INTO dentista (nome, email, senha) VALUES (?,?,?)'
-                        db.run(insert, ["Vitor","vitor@email.com", "vitor123"])
-                    }
-                });             
+            (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    var insert = 'INSERT INTO dentista (nome, email, senha) VALUES (?,?,?)'
+                    db.run(insert, ["Vitor", "vitor@email.com", "vitor123"])
+                }
+            });
     }
-    if(err) {
+    if (err) {
+        console.error(err.message);
+        throw err
+    } else {
+        db.run(
+            `CREATE TABLE userAdm (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome text, 
+                email text UNIQUE, 
+                senha text
+            )`,
+            (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    var insert = 'INSERT INTO userAdm (nome, email, senha) VALUES (?,?,?)'
+                    db.run(insert, ["Vitor adm", "vitor@adm.com", "vitor123"])
+                }
+            });
+    }
+    if (err) {
         console.error(err.message);
         throw err
     } else {
@@ -33,14 +53,14 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 data text, 
                 idDentista INTEGER
             )`,
-                (err) => {
-                    if (err) {
-                        console.log(err)
-                    }else{
-                        var insertMarcacao = 'INSERT INTO marcacao (data, idDentista) VALUES (?,?)'
-                        db.run(insertMarcacao, ['07/11/2021', 1])
-                    }
-                });             
+            (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    var insertMarcacao = 'INSERT INTO marcacao (data, idDentista) VALUES (?,?)'
+                    db.run(insertMarcacao, ['07/11/2021', 1])
+                }
+            });
     }
 });
 

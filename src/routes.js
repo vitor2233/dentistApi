7745b8c780396395fced4a6ffbcd9420f77e2fab
vitor2233 <vitorguilherme2233@gmail.com>
2021-11-22 22:26:53 -0300

    Validações de token

--- src/routes.js
+++ src/routes.js
@@ -11,33 +11,32 @@ const dentistaHasConsultorioController = require("./controllers/dentistaHasConsu
 const servicoController = require("./controllers/servicoController")
 
 // Dentista
-router.get("/", dentistaController.index)
-router.get("/api/dentistas", dentistaController.list)
-router.get("/api/dentista/:id", dentistaController.show)
-router.post("/api/dentista/login", dentistaController.login)
-router.post("/api/dentista", dentistaController.post)
-router.patch("/api/dentista/:id", dentistaController.update)
-router.delete("/api/dentista/:id", dentistaController.delete)
+router.get("/", verifyIfUserIsAdm, dentistaController.index)
+router.get("/api/dentistas", verifyIfUserIsAdm, dentistaController.list)
+router.get("/api/dentista/:id", verifyIfUserIsAdm, dentistaController.show)
+router.post("/api/dentista/login", verifyIfUserIsAdm, dentistaController.login)
+router.post("/api/dentista", verifyIfUserIsAdm, dentistaController.post)
+router.patch("/api/dentista/:id", verifyIfUserIsAdm, dentistaController.update)
+router.delete("/api/dentista/:id", verifyIfUserIsAdm, dentistaController.delete)
 
 //Consultório
-router.get("/api/consultorio", consultorioController.list)
-router.get("/api/consultorio/:id", consultorioController.show)
-router.post("/api/consultorio", consultorioController.post)
-router.patch("/api/consultorio/:id", consultorioController.update)
-router.delete("/api/consultorio/:id", consultorioController.delete)
+router.get("/api/consultorio", verifyIfUserIsAdm, consultorioController.list)
+router.get("/api/consultorio/:id", verifyIfUserIsAdm, consultorioController.show)
+router.post("/api/consultorio", verifyIfUserIsAdm, consultorioController.post)
+router.patch("/api/consultorio/:id", verifyIfUserIsAdm, consultorioController.update)
+router.delete("/api/consultorio/:id", verifyIfUserIsAdm, consultorioController.delete)
 
 //Serviço
-router.get("/api/servico", servicoController.list)
-router.get("/api/servico/:id", servicoController.show)
-router.post("/api/servico", servicoController.post)
-router.patch("/api/servico/:id", servicoController.update)
-router.delete("/api/servico/:id", servicoController.delete)
+router.get("/api/servico", verifyDentist, servicoController.list)
+router.get("/api/servico/:id", verifyDentist, servicoController.show)
+router.post("/api/servico", verifyDentist, servicoController.post)
+router.patch("/api/servico/:id", verifyDentist, servicoController.update)
+router.delete("/api/servico/:id", verifyDentist, servicoController.delete)
 
 //Dentista Consultorio
-router.get("/api/dentistaConsultorio", dentistaHasConsultorioController.list)
-router.post("/api/dentistaConsultorio", dentistaHasConsultorioController.post)
-router.patch("/api/dentistaConsultorio/:id", dentistaHasConsultorioController.update)
-router.delete("/api/dentistaConsultorio/:id", dentistaHasConsultorioController.delete)
+router.post("/api/dentistaConsultorio", verifyIfUserIsAdm, dentistaHasConsultorioController.post)
+router.patch("/api/dentistaConsultorio/:id", verifyIfUserIsAdm, dentistaHasConsultorioController.update)
+router.delete("/api/dentistaConsultorio/:id", verifyIfUserIsAdm, dentistaHasConsultorioController.delete)
 
 //Marcações
 router.get("/api/marcacoes", marcacoesController.list)
@@ -47,11 +46,11 @@ router.patch("/api/marcacao/:id", marcacoesController.update)
 router.delete("/api/marcacao/:id", marcacoesController.delete)
 
 // Usuário
-router.get("/api/usuario", verifyIfUserIsAdm, usuarioController.list)
+router.get("/api/usuario", usuarioController.list)
 router.get("/api/usuario/:id", usuarioController.show)
 router.post("/api/usuario/login", usuarioController.login)
 router.post("/api/usuario", usuarioController.post)
-router.patch("/api/usuario/:id", usuarioController.update)
-router.delete("/api/usuario/:id", usuarioController.delete)
+router.patch("/api/usuario/:id", verifyIfUserIsAdm, usuarioController.update)
+router.delete("/api/usuario/:id", verifyIfUserIsAdm, usuarioController.delete)
 
 module.exports = router
